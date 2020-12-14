5e47df30bddca23808e62e8415358b5aceff3546
Donuts_n_Tiaras <61537524+donutsntiaras@users.noreply.github.com>
2020-12-14 12:15:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function setup() {
+    video=createCapture(VIDEO);
+    video.size(550,550);
+
+    canvas=createCanvas(550, 550);
+    canvas.position(560, 300);
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("model is loaded");
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+    }
+}
